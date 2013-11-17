4e2e8c1c9f100d8dfcc01ecf4522ec4d96bfaab2
Dan Morrill <morrildl@sententio.us>
2013-11-16 23:10:07 -0800

    Rough cuts of most layout files.
    These are the XML layouts & associated assets for most of the screens the app
    needs. Very basic and currently unstyled. Code not currently written to
    populate/use these layouts.

--- android/Sproutly/src/main/java/dude/morrildl/sproutly/MainActivity.java
+++ android/Sproutly/src/main/java/dude/morrildl/sproutly/MainActivity.java
@@ -1,7 +1,6 @@
 package dude.morrildl.sproutly;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,20 +8,21 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.cultivar_create);
 
+        /*
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+        */
     }
 
 
@@ -57,8 +57,8 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            //View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return null;//rootView;
         }
     }
 
