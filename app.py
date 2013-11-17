9778abf0ad490a226d96c2f3850bbda717d3fca4
Aimee Morgan <aimeelmorgan@gmail.com>
2013-11-16 23:16:09 -0800

    GET and DELETE actions complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, request, jsonify, abort, make_response
 import model
 
 app = Flask(__name__)
@@ -9,83 +9,249 @@ def shutdown_session(exception=None):
     model.session.remove()
 
 
-# @app.route('/')
-# def index():
-# 	pass
+@app.route('/garden/api/plantings', methods=['GET'])
+def get_plantings():
+    results = model.session.query(model.Planting).all()
+    plantings = [{'id': d.id, 
+                  'name': d.name,
+                  'planted_date': serialize_date(d.planted_date),
+                  'completed': d.completed,
+                  'cultivar': d.cultivar_id,  
+                  'group': d.group_id,             
+                  'reminders': [f.id for f in d.reminders],
+                  'harvests': [f.id for f in d.harvests]} 
+                for d in results]
+    return jsonify(plantings=plantings)
 
-# @app.route('/garden/api/plantings', methods=['GET'])
-# def get_plantings():
-# 	plantings = model.session.query(model.Planting).all()
-# 	return jsonify(plantings)
+
+@app.route('/garden/api/plantings/<int:planting_id>', methods=['GET'])
+def get_planting():
+    result = model.session.query(model.Planting).get(planting_id)
+    if result == None:
+        abort(404)
+    planting = {'id': result.id, 
+                'name': result.name,
+                'planted_date': serialize_date(result.planted_date),
+                'completed': result.completed,
+                'cultivar': result.cultivar_id,  
+                'group': result.group_id,             
+                'reminders': [f.id for f in result.reminders],
+                'harvests': [f.id for f in result.harvests]}
+    return jsonify(planting=planting)
+
+
+@app.route('/garden/api/plantings', methods=['POST'])
+def create_planting():
+    if not request.json:
+        abort(400)
+    new_planting = model.Planting()
+    if 'name' in request.json and type(request.json['name']) != str:
+        abort(400)
+    new_planting.name = request.json.get('name', '')
+    planted_date_string = request.json.get('planted_date', '')
+    new_planting.planted_date = deserialize_date(planted_date_string)
+    new_planting.completed = False
+    if ('cultivar_id' in request.json and 
+        type(request.json['cultivar_id']) != int):
+        abort(400)
+    new_planting.cultivar_id = request.json.get('cultivar_id', '')
+    if ('group_id' in request.json and 
+        type(request.json['group_id']) != int):
+        abort(400)
+    new_planting.group_id = request.json.get('group_id', '')
+    model.session.add(new_planting)
+    model.session.commit()
+    return 201
+
+
+@app.route('/garden/api/plantings/<int:planting_id>', methods=['DELETE'])
+def delete_planting(planting_id):
+    planting = model.session.query(model.Planting).get(planting_id)
+    if planting == None:
+        abort(404)
+    model.session.delete(planting)
+    model.session.commit()
+    return jsonify({'result': True})
+
 
 @app.route('/garden/api/groups', methods=['GET'])
 def get_groups():
-	results = model.session.query(model.Group).all()
-	groups_list = [{'id': d.id, 'location': d.location} for d in results]
-	return jsonify(groups = groups_list)
+    results = model.session.query(model.Group).all()
+    groups = [{'id': d.id, 'location': d.location} for d in results]
+    return jsonify(groups=groups)
 
 
-@app.route('/garden/api/group/<int:group_id>', methods=['GET'])
+@app.route('/garden/api/groups/<int:group_id>', methods=['GET'])
 def get_group(group_id):
-	group = model.session.query(model.Group).get(group_id)
-	return jsonify(id=group.id, location=group.location)
+    group = model.session.query(model.Group).get(group_id)
+    if group == None:
+        abort(404)
+    return jsonify(id=group.id, location=group.location)
+
+
+@app.route('/garden/api/groups', methods=['POST'])
+def create_group():
+    if not request.json or 'location' not in request.json:
+        abort(400)
+    new_group = model.Group()
+    if type(request.json['location']) != str:
+        abort(400)
+    new_group.location = request.json['location']
+    model.session.add(new_group)
+    model.session.commit()
+    return 201
+
+
+@app.route('/garden/api/groups/<int:group_id>', methods=['DELETE'])
+def delete_group(group_id):
+    group = model.session.query(model.Group).get(group_id)
+    if group == None:
+        abort(404)
+    model.session.delete(group)
+    model.session.commit()
+    return jsonify({'result': True})
 
 
 @app.route('/garden/api/reminders', methods=['GET'])
 def get_reminders():
-	results = model.session.query(model.Reminder).all()
-	reminders_list = [{'id': d.id, 
-					   'due_date': serialize_date(d.due_date),
-					   'kind': d.kind,
-					   'text': d.text,
-					   'completed_date': serialize_date(d.completed_date),
-					   'recurrence': d.recurrence,
-					   'planting_id': d.planting_id} for d in results]
-	return jsonify(reminders = reminders_list)
+    results = model.session.query(model.Reminder).all()
+    reminders_list = [{'id': d.id, 
+                       'due_date': d.due_date,
+                       'kind': d.kind,
+                       'text': d.text,
+                       'completed_date': serialize_date(d.completed_date),
+                       'recurrence': d.recurrence,
+                       'planting_id': d.planting_id} for d in results]
+    return jsonify(reminders=reminders_list)
 
 
-@app.route('/garden/api/reminder/<int:reminder_id>', methods=['GET'])
+@app.route('/garden/api/reminders/<int:reminder_id>', methods=['GET'])
 def get_reminder(reminder_id):
-	reminder = model.session.query(model.Reminder).get(reminder_id)
-	due_date = serialize_date(reminder.due_date)
-	return jsonify(id=reminder.id,
-				   due_date= due_date,
-				   kind=reminder.kind,
-				   text=reminder.text,
-				   completed_date=reminder.completed_date,
-				   recurrence=reminder.recurrence,
-				   planting_id=reminder.planting_id)
+    reminder = model.session.query(model.Reminder).get(reminder_id)
+    if reminder == None:
+        abort(404)
+    return jsonify(id=reminder.id,
+                   due_date= due_date,
+                   kind=reminder.kind,
+                   text=reminder.text,
+                   completed_date=reminder.completed_date,
+                   recurrence=reminder.recurrence,
+                   planting_id=reminder.planting_id)
 
 
-# app.route('/garden/api/cultivars', methods=['GET'])
-# def get_cultivars():
-# 	results = model.session.query(model.Cultivars).all()
-# 	cultivars_list = [{'id': d.id, 
-# 					   'due_date': d.due_date,
-# 					   'kind': d.kind,
-# 					   'text': d.text,
-# 					   'completed': d.completed,
-# 					   'recurrence': d.recurrence,
-# 					   'planting_id': d.planting_id} for d in results]
-# 	return jsonify(cultivars = cultivars_list)
+@app.route('/garden/api/reminders/<int:reminder_id>', methods=['DELETE'])
+def delete_reminder(reminder_id):
+    reminder = model.session.query(model.Reminder).get(reminder_id)
+    if reminder == None:
+        abort(404)
+    model.session.delete(reminder)
+    model.session.commit()
+    return jsonify({'result': True})
 
 
-# @app.route('/garden/api/cultivars/<int:cultivar_id>', methods=['GET'])
-# def get_cultivar(reminder_id):
-# 	reminder = model.session.query(model.Reminder).get(reminder_id)
-# 	return jsonify(id=reminder.id,
-# 				   due_date= reminder.due_date,
-# 				   kind=reminder.kind,
-# 				   text=reminder.text,
-# 				   completed=reminder.completed,
-# 				   recurrence=reminder.recurrence,
-# 				   planting_id=reminder.planting_id)
+@app.route('/garden/api/cultivars', methods=['GET'])
+def get_cultivars():
+    results = model.session.query(model.Cultivar).all()
+    cultivars = [{'id': d.id, 
+                  'name': d.name,
+                  'type': d.cultivar_type,
+                  'purchase_date': serialize_date(d.purchase_date),
+                  'manufacturer': d.manufacturer,
+                  'source': d.source,
+                  'form': d.form,
+                  'days_to_harvest': d.days_to_harvest,
+                  'plantings': [f.id for f in d.plantings]} 
+                 for d in results]
+    return jsonify(cultivars=cultivars)
+
+
+@app.route('/garden/api/cultivar/<int:cultivar_id>', methods=['GET'])
+def get_cultivar(cultivar_id):
+    result = model.session.query(model.Cultivar).get(cultivar_id)
+    if result == None:
+        abort(404)
+    cultivar = {'id': result.id, 
+                'name': result.name,
+                'type': result.cultivar_type,
+                'purchase_date': serialize_date(result.purchase_date),
+                'manufacturer': result.manufacturer,
+                'source': result.source,
+                'form': result.form,
+                'days_to_harvest': result.days_to_harvest,
+                'plantings': [f.id for f in result.plantings]} 
+    return jsonify(cultivar=cultivar)
+
+
+@app.route('/garden/api/cultivars/<int:cultivar_id>', methods=['DELETE'])
+def delete_cultivar(cultivar_id):
+    cultivar = model.session.query(model.Cultivar).get(cultivar_id)
+    if cultivar == None:
+        abort(404)
+    model.session.delete(cultivar)
+    model.session.commit()
+    return jsonify({'result': True})
+
+
+@app.route('/garden/api/harvests', methods=['GET'])
+def get_harvests():
+    results = model.session.query(model.Harvest).all()
+    harvests = [{'id': d.id, 
+                 'harvest_date': serialize_date(d.harvest_date),
+                 'quantity': d.quantity,
+                 'units': d.units,
+                 'notes': d.notes,
+                 'next_year': d.next_year,
+                 'planting_id': d.planting_id} 
+               for d in results]
+    return jsonify(harvests=harvests)
+
+
+@app.route('/garden/api/harvests/<int:harvest_id>', methods=['GET'])
+def get_harvest(harvest_id):
+    result = model.session.query(model.Harvest).get(harvest_id)
+    if result == None:
+        abort(404)
+    harvest = {'id': result.id, 
+               'harvest_date': serialize_date(result.harvest_date),
+               'quantity': result.quantity,
+               'units': result.units,
+               'notes': result.notes,
+               'next_year': result.next_year,
+               'planting_id': result.planting_id} 
+    return jsonify(harvest=harvest)   
+
+
+@app.route('/garden/api/harvests/<int:harvest_id>', methods=['DELETE'])
+def delete_harvest(harvest_id):
+    harvest = model.session.query(model.Harvest).get(harvest_id)
+    if harvest == None:
+        abort(404)
+    model.session.delete(harvest)
+    model.session.commit()
+    return jsonify({'result': True})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Not found' } ), 404)
+
+
+@app.errorhandler(400)
+def not_found(error):
+    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
 
 
 def serialize_date(date):
-	if date == None:
-		return None
-	return '%i, %i, %i' % (date.year, date.month, date.day)
+    if date == None:
+        return None
+    return '%i, %i, %i' % (date.year, date.month, date.day)
+
+
+def deserialize_date(datestring):
+    if not datestring:
+        return None
+    datelist = datestring.split("'")
+    return Date(datelist[0], datelist[1], datelist[2])
 
 
 if __name__ == '__main__':
--- model.py
+++ model.py
@@ -12,61 +12,66 @@ Base = declarative_base()
 Base.query = session.query_property()
 
 class Planting(Base):
-	__tablename__ = 'plantings'
+    __tablename__ = 'plantings'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(256), nullable=False)
-	planted_date = Column(Date)
-	completed = Column(Boolean, default=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    planted_date = Column(Date)
+    completed = Column(Boolean, default=False)
 
-	reminders = relationship('Reminder', backref='planting')
-	cultivar_id = Column(Integer, ForeignKey('cultivars.id'))
-	cultivar = relationship('Cultivar', backref='plantings')
-	harvests = relationship('Harvest', backref='planting')
-	group = relationship('Group', backref='plantings')
-	group_id = Column(Integer, ForeignKey('groups.id'))
+    reminders = relationship('Reminder', backref='planting')
+    cultivar_id = Column(Integer, ForeignKey('cultivars.id'))
+    cultivar = relationship('Cultivar', backref='plantings')
+    harvests = relationship('Harvest', backref='planting')
+    group = relationship('Group', backref='plantings')
+    group_id = Column(Integer, ForeignKey('groups.id'))
+    # need to ensure that when a planting is completed, its reminders
+    # get expired. also, if a planting gets deleted, its reminders
+    # and harvests should get deleted
 
 
 class Reminder(Base):
-	__tablename__ = 'reminders'
+    __tablename__ = 'reminders'
 
-	id = Column(Integer, primary_key=True)
-	due_date = Column(Date, nullable=False)
-	kind = Column(Enum('water', 'fertilize', 'harvest', name='reminder_type',
-			nullable=False))
-	text = Column(String(256))
-	completed_date = Column(Date, default=None)
-	recurrence = Column(Integer)
-	planting_id = Column(Integer, ForeignKey('plantings.id'))
+    id = Column(Integer, primary_key=True)
+    due_date = Column(Integer, nullable=False)
+    kind = Column(Enum('water', 'fertilize', 'harvest', 'pesticide',
+            name='reminder_type',
+            nullable=False))
+    text = Column(String(256))
+    completed_date = Column(Date, default=None)
+    recurrence = Column(Integer)
+    planting_id = Column(Integer, ForeignKey('plantings.id'))
 
 
 class Cultivar(Base):
-	__tablename__ = 'cultivars'
+    __tablename__ = 'cultivars'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(256), nullable=False)
-	cultivar_type = Column(Enum('tomato', 'broccoli', 'squash', 'lettuce', 
-					'other', name="type"), nullable=False)
-	purchase_date = Column(Date, nullable=True)
-	source = Column(String(256), nullable=True)
-	manufacturer = Column(String(256), nullable=True)
-	form = Column(Enum('seed', 'seedling', 'bulb', name='form'))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256), nullable=False)
+    cultivar_type = Column(Enum('tomato', 'broccoli', 'squash', 'lettuce', 
+                    'other', name="type"), nullable=False)
+    purchase_date = Column(Date, nullable=True)
+    source = Column(String(256), nullable=True)
+    manufacturer = Column(String(256), nullable=True)
+    form = Column(Enum('seed', 'seedling', 'bulb', name='form'))
+    days_to_harvest = Column(Integer)
 
 
 class Harvest(Base):
-	__tablename__ = 'harvests'
+    __tablename__ = 'harvests'
 
-	id = Column(Integer, primary_key=True)
-	harvest_date = Column(Date)
-	quantity = Column(Float)
-	units = Column(String(128))
-	notes = Column(String(512))
-	next_year = Column(String(512))
-	planting_id = Column(Integer, ForeignKey('plantings.id'))
+    id = Column(Integer, primary_key=True)
+    harvest_date = Column(Date)
+    quantity = Column(Float)
+    units = Column(String(128))
+    notes = Column(String(512))
+    next_year = Column(String(512))
+    planting_id = Column(Integer, ForeignKey('plantings.id'))
 
 
 class Group(Base):
-	__tablename__ = 'groups'
+    __tablename__ = 'groups'
 
-	id = Column(Integer, primary_key=True)
-	location = Column(String(256), nullable=False)
+    id = Column(Integer, primary_key=True)
+    location = Column(String(256), nullable=False)
